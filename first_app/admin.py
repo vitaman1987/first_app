985f4a91d15dc45c09fa32d8f3e803aaf33d0f95
vitaman1987 <vitaman1987@gmail.com>
2017-11-02 21:21:36 +0200

    models initials

--- first_app/admin.py
+++ first_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from first_app.models import Topic, AccassRecord, Webpage
 # Register your models here.
+
+admin.site.register(AccassRecord)
+admin.site.register(Topic)
+admin.site.register(Webpage)
--- first_app/views.py
+++ first_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Topic, Webpage, AccassRecord
 
 def index(request):
-    my_dict = {'insert_me':'Hello I am coming from first_app/index.html!'}
-    return render(request, 'first_app/index.html', context = my_dict)
+    webpages_list = AccassRecord.objects.order_by('date')
+    date_dict = {'acces_records': webpages_list}
+    return render(request, 'first_app/index.html', context = date_dict)
